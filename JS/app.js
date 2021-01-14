e2b0cf44829f01679020e93f1cb37b83d0765e6b
Wilber <wilberherrera0422@gmail.com>
2021-01-13 22:18:36 -0430

    Register

---
+++ JS/app.js
@@ -0,0 +1,15 @@
+var x = document.getElementById("Login")
+        var y = document.getElementById("Register")
+        var z = document.getElementById("Btn")
+
+        function Register(){
+            x.style.left = "-400px";
+            y.style.left = "50px";
+            z.style.left = "110px";
+        }
+
+        function Login(){
+            x.style.left = "50px";
+            y.style.left = "400px";
+            z.style.left = "0";
+        }
